32872f20555e9304942f3081b6947fe0de9a1275
Ayres Roux <ayresrouxio@gmail.com>
2024-12-12 19:04:12 -0300

    aula: Sintaxe

--- scripts.js
+++ scripts.js
@@ -1,16 +1,8 @@
-// O console.log exibe no console um texto.
-console.log('Ayres Roux') // Também posso comentar do lado.
+// Sintaxe correta.
+console.log("Olá Rodrigo!")
 
-/**
- * Comentário de múltiplas linhas.
- * Comentários são trecho de texto que podem ser adicionados
- * junto com o código.
- *
- * O navegador ignora o text marcado como comentário.
- *
- * Porque utilizar comentários durante seus estudos:
- * - Oferecer informações adicionais sobre o que o código faz;
- * - Deixar lembretes de coisas a fazer;
- * - Relembrar no futuro o que aquilo faz;
- * - Etc...
- */
+// Sintaxe incorreta.
+// con.sole.log("Olá Rodrigo!")
+
+// No javascript ponto e vírgula é opcional.
+console.log("Olá João!")
