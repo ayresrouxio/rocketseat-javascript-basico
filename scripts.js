b9ff33ac6f82533e69417449a41902da74a502e5
Ayres Roux <ayresrouxio@gmail.com>
2024-12-12 18:58:15 -0300

    aula: Comentários em Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log('Ayres Roux')
+// O console.log exibe no console um texto.
+console.log('Ayres Roux') // Também posso comentar do lado.
+
+/**
+ * Comentário de múltiplas linhas.
+ * Comentários são trecho de texto que podem ser adicionados
+ * junto com o código.
+ *
+ * O navegador ignora o text marcado como comentário.
+ *
+ * Porque utilizar comentários durante seus estudos:
+ * - Oferecer informações adicionais sobre o que o código faz;
+ * - Deixar lembretes de coisas a fazer;
+ * - Relembrar no futuro o que aquilo faz;
+ * - Etc...
+ */
