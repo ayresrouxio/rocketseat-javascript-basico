b8d1cc510eaa21884c1c510d673e3032ee5e9366
Ayres Roux <ayresrouxio@gmail.com>
2024-12-12 21:07:29 -0300

    aula: String

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let username = "Rodrigo"
+console.log(username)
+console.log(typeof username)
+
+// Quanto utilizar uma ou outra?
+console.log('Uma string com "aspas duplas" dentro de simples.')
+console.log("Uma string com 'aspas simples' dentro de dupla.")
+console.log(`
+  Uma string com acento grave permite
+  escrever múltiplas linhas.
+`)
