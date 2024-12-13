1c32d3c702af513cf222f5672cdde8dfa2260d36
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 06:39:11 -0300

    aula: Argumentos e Parâmetros

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
-function message() {
-  console.log("Olá. É bom ter você aqui!")
+/*
+  - Parâmetros: é a variável (escopo da função) que irá
+  receber um valor em uma função.
+  - Argumentos: é o valor que é passado para a função.
+*/
+
+// Passando parâmetro username.
+function message(username) {
+  console.log("Olá", username)
 }
 
-message()
-message()
+// Passando argumentos.
+message("Rodrigo")
+message("Ana")
+
+function sum(a, b) {
+  console.log(a + b)
+}
+
+sum(10, 20)
+sum(23, 18)
+
+// Definindo um valor (argumento) padrão.
+function joinText(text1, text2 = "", text3 = "") {
+  console.log(text1, text2, text3)
+}
+
+joinText("Rodrigo", "Gonçalves", "Santana")
+joinText("Gonçalves", "Rodrigo", "Santana")
+
+joinText("Rodrigo")
