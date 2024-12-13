017e83f4d61ccdf7d8c9378fbe3c231849672a27
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 06:05:28 -0300

    aula: Else If

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
-// if else (se, senão)
+// If Else If
 
-let age = 17
+let hour = 11
 
-if (age < 18) {
-  console.log("Você não pode dirigir")
+/*
+if (hour <= 12) {
+  console.log("Bom dia")
+} else if (hour > 18) {
+  console.log("Boa noite")
+} else if (hour > 12) {
+  console.log("Boa tarde")
+}
+*/
+
+if (hour <= 12) {
+  console.log("Bom dia")
+} else if (hour > 12 && hour <= 18) {
+  console.log("Boa tarde")
 } else {
-  console.log("Você pode dirigir")
+  console.log("Boa noite")
 }
