f826990583411444c5ab480c77ed911d6fab0044
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 06:43:01 -0300

    aula: Retornando valores

--- scripts.js
+++ scripts.js
@@ -1,31 +1,9 @@
-/*
-  - Parâmetros: é a variável (escopo da função) que irá
-  receber um valor em uma função.
-  - Argumentos: é o valor que é passado para a função.
-*/
-
-// Passando parâmetro username.
-function message(username) {
-  console.log("Olá", username)
-}
-
-// Passando argumentos.
-message("Rodrigo")
-message("Ana")
-
 function sum(a, b) {
-  console.log(a + b)
+  let result = a + b
+  return result
 }
 
-sum(10, 20)
-sum(23, 18)
+let response = sum(7, 8)
+console.log(response)
 
-// Definindo um valor (argumento) padrão.
-function joinText(text1, text2 = "", text3 = "") {
-  console.log(text1, text2, text3)
-}
-
-joinText("Rodrigo", "Gonçalves", "Santana")
-joinText("Gonçalves", "Rodrigo", "Santana")
-
-joinText("Rodrigo")
+console.log(sum(5, 6))
