ed7c43c4de30c6c2f5d9698d8e80791a8d587194
Ayres Roux <ayresrouxio@gmail.com>
2024-12-12 21:23:56 -0300

    aula: Boolean

--- scripts.js
+++ scripts.js
@@ -1,14 +1,6 @@
-// Number
-console.log(typeof 5)
+// Boolean
+console.log(true)
+console.log(false)
 
-// Inteiro positivo.
-console.log(5)
-
-// Inteiro negativo.
-console.log(-5)
-
-// Inteiro reais ou float.
-console.log(7.5)
-
-// NaN - Not a Number.
-console.log(12.5 / "rodrigo")
+let isLoading = true
+console.log(typeof isLoading)
