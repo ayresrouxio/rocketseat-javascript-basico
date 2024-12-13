e9b81bba3dd45607f04aab3c768b016050f4de01
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 11:13:52 -0300

    aula: Query Selector

--- scripts.js
+++ scripts.js
@@ -1,24 +1,10 @@
-// Visualizar o conteúdo do document.
-// console.log(document)
-
-// Obter o title da página.
-console.log(document.title)
-
-// Acessar o elemento pelo ID (seletor ID).
-const guest = document.getElementById("guest-1")
+// Acessa o elemento pelo seletor ID.
+const guest = document.querySelector("#guest-1")
 console.log(guest)
 
-// Mostra as propriedades do objeto.
-console.dir(guest)
+// Acessa o elemento pelo seletor CLASS.
+// const guests = document.querySelector(".guest")
 
-// Acessar elemento com class (seletor class).
-const guestsByClass = document.getElementsByClassName("guest")
-console.log(guestsByClass)
-
-// Exibir o primeiro elemento da lista.
-console.log(guestsByClass.item(0))
-console.log(guestsByClass[1])
-
-// Selecionar lista de elementos pela tag.
-const guestsByTag = document.getElementsByTagName("li")
-console.log(guestsByTag)
+// Retorna todos os elementos encontrados pela query.
+const guests = document.querySelectorAll(".guest")
+console.log(guests)
