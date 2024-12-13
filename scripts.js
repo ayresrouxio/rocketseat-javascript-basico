f399a0aacb3a86670d67e5ef8d947f88c4a33720
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 05:50:44 -0300

    aula: Falsy e Truthy

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
-// Operador condicional ternário
+/*
+  FALSY quando um valor é considerado false e TRUTHY quando
+  é considerado verdadeiro em contextos onde um boolean é obrigatório
+  (condicionais e loops).
+*/
 
-let age = 21
+console.log("Exemplos de Falsy")
 
-console.log(
-  age >= 18 // condição
-    ? "Você pode dirigir." // se verdadeiro
-    : "Você não pode dirigir." // se falso
-)
+console.log(false ? "Verdadeiro" : "Falso")
+console.log(0 ? "Verdadeiro" : "Falso")
+console.log(-0 ? "Verdadeiro" : "Falso")
+console.log("" ? "Verdadeiro" : "Falso")
+console.log(null ? "Verdadeiro" : "Falso")
+console.log(undefined ? "Verdadeiro" : "Falso")
+console.log(NaN ? "Verdadeiro" : "Falso")
+
+console.log("Exemplos de Truthy")
+
+console.log(true ? "Verdadeiro" : "Falso")
+console.log({} ? "Verdadeiro" : "Falso")
+console.log([] ? "Verdadeiro" : "Falso")
+console.log(1 ? "Verdadeiro" : "Falso")
+console.log(3.2 ? "Verdadeiro" : "Falso")
+console.log("A" ? "Verdadeiro" : "Falso")
+console.log(" " ? "Verdadeiro" : "Falso")
+console.log("false" ? "Verdadeiro" : "Falso")
+console.log(-1 ? "Verdadeiro" : "Falso")
+console.log(Infinity ? "Verdadeiro" : "Falso")
+console.log(-Infinity ? "Verdadeiro" : "Falso")
