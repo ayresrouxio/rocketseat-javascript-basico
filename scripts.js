bb71d7919451fb5dedc0c96d8d30269fc87a65a6
Ayres Roux <ayresrouxio@gmail.com>
2024-12-12 22:28:21 -0300

    aula: Operadores Aritméticos

--- scripts.js
+++ scripts.js
@@ -1,31 +1,14 @@
-/*
- - Conversão de tipos (type casting ou type conversion):
- ocorre quando você explicitamente transforma um valor de um
- tipo para outro. Isso é feito de forma consciente, usando
- funções ou métodos específicos para realizar a conversão.
-*/
+// Operadores Aritméticos
+console.log("Soma: ", 12 + 2)
 
-let value = "9"
-console.log(typeof value)
-console.log(typeof Number(value))
+console.log("Concatenação: ", "12" + "8")
 
-let age = 18
-console.log(typeof age)
-console.log(typeof age.toString())
-console.log(typeof String(age))
+console.log("Subtração: ", 12 - 9)
 
-let option = 1
-console.log(Boolean(option))
-console.log(typeof option)
-console.log(typeof Boolean(option))
+console.log("Multiplicação: ", 3 * 8)
 
-/*
-  - Coerção de tipos:
-  Acontece de forma automática (implicitamente). O JavaScript
-  tenta automaticamente converter um dos valores para um tipo
-  compatível antes de realizar a operação.
-*/
+console.log("Divisão: ", 12 / 3)
 
-// Exemplo de coerção.
-console.log("10" + 5)
-console.log(typeof ("10" + 5))
+console.log("Resto da divisão: ", 12 % 5)
+
+console.log("Exponencial: ", 3 ** 3)
