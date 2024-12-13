7c77edcca19c05c3768293aed2bf1777e11fb3e8
Ayres Roux <ayresrouxio@gmail.com>
2024-12-13 06:56:29 -0300

    aula: Comentário de Documentação

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-showMessage("Olá, Rodrigo!")
+/*
+  Comentário de documentação em Javascript (sintaxe do JSDoc).
+  O JSDoc é uma padrão para incorporar documentação no código-fonte a
+  partir desses comentários.
+*/
 
-function showMessage(message) {
-  console.log(message)
-  endLine()
-
-  function endLine() {
-    console.log("----------------")
-  }
+/**
+ * Authenticates the user.
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+ */
+function signIn(email, password) {
+  // Fluxo de autenticação do usuário.
+  return 7
 }
 
-showMessage("Tudo bem?")
-
-// Não existe nesse escopo
-endLine()
+signIn("rodrigo@email.com", "2134567")
